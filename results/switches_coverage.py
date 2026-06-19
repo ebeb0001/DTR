74a20639782c851d16783323652dcaa0edb2ba6e
ebeb0001 <ethan.bebelambou.nkoli@ulb.be>
2026-06-19 14:05:55 +0200

    switches coverage without junctions

--- results/switches_coverage.py
+++ results/switches_coverage.py
@@ -1,12 +1,25 @@
 import pandas as pd
 reference_switches_number = 10379
-simulation_data = pd.read_csv('sumo_data/switches.csv', delimiter=';').shape[0]
+
+tracks = pd.read_csv('sumo_data/main_tracks.csv', delimiter=';')
+switches = dict()
+for _, row in tracks.iterrows():
+    start = row['Departure_switch']
+    end = row['Arrival_switch']
+    if start != end:
+        if start not in switches:
+            switches[start] = 0
+        switches[start] += 1
+        if end not in switches:
+            switches[end] = 0
+        switches[end] += 1
+real_switches = [s for s in switches if switches[s] > 1]
 
 print("Switches Coverage:")
 print("==========================")
 print('Reference data:')
 print(f"{reference_switches_number} switches")
 print('Simulation data:')
-print(f"{simulation_data} switches")
+print(f"{len(real_switches)} switches")
 print('Coverage:')
-print(f'{simulation_data / reference_switches_number * 100:.2f}%')
+print(f'{len(real_switches) / reference_switches_number * 100:.2f}%')
